42ad3ede89e46db388457cb0344cd8dcb3f8d440
NickZhezl <zhelezko.nik12345@gmail.com>
2025-10-29 09:10:48 +0100

    feat: added task 1

--- main/task-1.py
+++ main/task-1.py
@@ -1,9 +1,7 @@
-# import numpy as np
-# arr = np.random.randint(0, 10, 10)
-# print(arr)
-# for num,val in enumerate(arr):
-#     if val in range(3,9):
-#         arr[num] = val - val*2
-# print(arr)
-#
-#
+import numpy as np
+arr = np.random.randint(0, 10, 10)
+print('Исходный масс', arr)
+
+mask = (arr >= 3) & (arr < 8)
+arr[mask] = -arr[mask]
+print('New масс', arr)
