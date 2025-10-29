518920947a9006e0c34984430e82f0eed23a4d22
NickZhezl <zhelezko.nik12345@gmail.com>
2025-10-29 09:05:07 +0100

    init: initial commit

---
+++ main/task-1.py
@@ -0,0 +1,9 @@
+# import numpy as np
+# arr = np.random.randint(0, 10, 10)
+# print(arr)
+# for num,val in enumerate(arr):
+#     if val in range(3,9):
+#         arr[num] = val - val*2
+# print(arr)
+#
+#
